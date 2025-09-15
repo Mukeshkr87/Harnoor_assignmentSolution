56154b8ac9a55dc295a89577591eb2a8e10cf69b
Mukeshkr87 <msk845455@gmail.com>
2025-09-16 01:17:41 +0530

    JWt assignment completed

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
+const zod = require('zod');
 const jwtPassword = 'secret';
-
+    const schema = zod.object({
+        username: zod.string().email(),
+        password: zod.string().min(6)
+    })
+    
 
 /**
  * Generates a JWT for a given username and password.
@@ -15,6 +20,13 @@ const jwtPassword = 'secret';
  */
 function signJwt(username, password) {
     // Your code here
+    const result = schema.safeParse({username,password});
+    if(result.success){
+        return jwt.sign({username,password},jwtPassword);
+    }else{
+        return null;
+    }
+
 }
 
 /**
@@ -27,6 +39,12 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    try{
+        jwt.verify(token,jwtPassword);
+        return true;
+    }catch(err){
+        return false;
+    }
 }
 
 /**
@@ -38,6 +56,12 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decode = jwt.decode(token);
+    if(decode){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
